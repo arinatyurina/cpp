ef59e56f200328c8bc0f96ac4fc915f452a9da1d
Arina Tyurina <atyurina@student.42london.com>
2024-06-05 18:46:14 +0100

    ex00

---
+++ ex01/header.hpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   header.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: atyurina <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/05 16:57:54 by atyurina          #+#    #+#             */
+/*   Updated: 2024/06/05 16:57:56 by atyurina         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+private members of a class are accessible only from within other members of the same class (or from their "friends").
+protected members are accessible from other members of the same class (or from their "friends"), but also from members of their derived classes.
+Finally, public members are accessible from anywhere where the object is visible.
+*/
+
+#ifndef HEADER_H
+# define HEADER_H
+
+class PhoneBook {
+private:
+
+public:
+	(char*);
+};
+
+#endif
