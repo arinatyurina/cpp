03dfe077256cb4f11b3ff7aaf827599b48b500e7
Arina <atyurina@student.42london.com>
2024-06-13 18:32:20 +0100

    class attributes added

--- ex01/header.hpp
+++ ex01/header.hpp
@@ -11,6 +11,7 @@
 /* ************************************************************************** */
 
 /*
+visibility:
 private members of a class are accessible only from within other members of the same class (or from their "friends").
 protected members are accessible from other members of the same class (or from their "friends"), but also from members of their derived classes.
 Finally, public members are accessible from anywhere where the object is visible.
@@ -19,11 +20,56 @@ Finally, public members are accessible from anywhere where the object is visible
 #ifndef HEADER_H
 # define HEADER_H
 
-class PhoneBook {
-private:
+#include <iostream>
+//class, attributes and functions it contains
+/*classes allow us to group variables together into a type
+and also add functionality to those variables*/
 
+class Contact
+{
+private:
+	int			id;
+	std::string	first_name;
+	std::string	first_name;
+	std::string	nickname;
+	std::string	phone_number;
+	std::string	darkest_secret;
 public:
-	(char*);
+	Contact(/* args */);
+	~Contact();
 };
+
+Contact::Contact(/* args */)
+{
+}
+
+Contact::~Contact()
+{
+}
+
+class PhoneBoook
+{
+private:
+	/* data */
+	Contact	contact[8];
+	int		id;
+	int		amount;
+public:
+	PhoneBoook(/* args */);
+	~PhoneBoook();
+	
+	void	add(Contact Contact);
+	void	search(PhoneBoook PhoneBoook);
+	void	exit(void);
+};
+
+//cpnstructors and destructors do not returt anything
+PhoneBoook::PhoneBoook(/* args */)
+{
+}
+
+PhoneBoook::~PhoneBoook()
+{
+}
 
 #endif
