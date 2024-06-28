d43ac3eea2dba3fb303d89b804dcd5f6a6c42511
Arina <atyurina@student.42london.com>
2024-06-28 13:59:40 +0100

    message style

--- module_01/ex05/Harl.cpp
+++ module_01/ex05/Harl.cpp
@@ -6,7 +6,7 @@
 /*   By: atyurina <atyurina@student.42london.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/27 20:51:34 by atyurina          #+#    #+#             */
-/*   Updated: 2024/06/28 13:19:13 by atyurina         ###   ########.fr       */
+/*   Updated: 2024/06/28 13:58:26 by atyurina         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,22 @@
 
 void    Harl::debug(void)
 {
-	std::cout << "That's a debug message" << std::endl;
+	std::cout << ">> That's a debug message" << std::endl;
 }
 
 void	Harl::info(void)
 {
-	std::cout << "That's an info message" << std::endl;
+	std::cout << ">> That's an info message" << std::endl;
 }
 
 void	Harl::warning(void)
 {
-	std::cout << "That's a warning message" << std::endl;
+	std::cout << ">> That's a warning message" << std::endl;
 }
 
 void	Harl::error(void)
 {
-	std::cout << "That's an error message" << std::endl;
+	std::cout << ">> That's an error message" << std::endl;
 }
 
 void	Harl::complain(std::string level)
