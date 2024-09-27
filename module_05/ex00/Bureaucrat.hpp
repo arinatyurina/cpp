2068de0e203342a3875edef78fe83e9b570a9414
Arina <atyurina@student.42london.com>
2024-09-27 16:31:21 +0100

    m05 ex00

---
+++ module_05/ex00/Bureaucrat.hpp
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: atyurina <atyurina@student.42london.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/27 13:07:35 by atyurina          #+#    #+#             */
+/*   Updated: 2024/09/27 16:21:44 by atyurina         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string>
+#include <iostream>
+#include <exception>
+
+class Bureaucrat
+{
+private:
+	std::string const	name;
+	int	grade;
+public:
+	Bureaucrat();
+	Bureaucrat(std::string name, int grade);
+	~Bureaucrat();
+	Bureaucrat(const Bureaucrat& other);
+	Bureaucrat& operator=(const Bureaucrat& other);
+
+	std::string const	&getName() const;
+	int	getGrade() const;
+	
+	void	incrementGrade();
+	void	decrementGrade();
+
+	class GradeTooHighException : public std::exception
+	{
+		public:
+		const char* what() const throw()
+		{
+			return ("Grade is too High");
+		}
+	};
+	class GradeTooLowException : public std::exception
+	{
+		public:
+		const char* what() const throw()
+		{
+			return ("Grade is too Low");
+		}
+	};
+};
+
+std::ostream& operator<<(std::ostream& stream, const Bureaucrat& b);
---
+++ module_05/ex00/main.cpp
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: atyurina <atyurina@student.42london.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/27 13:07:27 by atyurina          #+#    #+#             */
+/*   Updated: 2024/09/27 16:30:16 by atyurina         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Bureaucrat.hpp"
+
+int	main()
+{
+	/*Instatiation with an invalide grade*/
+	try
+	{
+		Bureaucrat	John("John", 155);
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << "Exception: " << e.what() << '\n';
+	}
+	Bureaucrat	Alice("Alice", 150);
+	try
+	{
+		Alice.incrementGrade();
+		std::cout << Alice;
+		Alice.decrementGrade();
+		std::cout << Alice;
+		Alice.decrementGrade();
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << "Exception: " << e.what() << '\n';
+	}
+}
+
+/*
+Exception are used to handle errors during execution
+throw is redundant (it breaks the executioin of function)
+try
+{
+	code to be executed
+}
+catch (type of exception - const char* mes)
+{
+	cout << mes << endl;
+}
+catch(...){} - handling any type of exception
+ */
