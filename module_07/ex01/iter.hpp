a3d8d55d7e08a28a70b5f9fcaf888a42e664f79a
Arina <atyurina@student.42london.com>
2026-01-13 20:03:59 +0000

    added const handling

--- module_07/ex01/iter.hpp
+++ module_07/ex01/iter.hpp
@@ -6,7 +6,7 @@
 /*   By: atyurina <atyurina@student.42london.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/14 20:22:03 by atyurina          #+#    #+#             */
-/*   Updated: 2024/10/14 20:47:45 by atyurina         ###   ########.fr       */
+/*   Updated: 2026/01/12 21:49:13 by atyurina         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,21 @@
 #include <string>
 #include <cctype>
 
-template <typename T, typename F>
-void iter(T* arr, int len, F func)
+template <typename T>
+void iter(T* arr, size_t len, void (*func)(T&))
 {
-	for (int i = 0; i < len; i++)
+//	std::cout << "Template with non-const reference to function was called:\n";
+	for (size_t i = 0; i < len; i++)
 		func(arr[i]);
 }
+
+template <typename T>
+void iter(T* arr, size_t len, void (*func)(const T&))
+{
+//	std::cout << "Template with const reference to function was called:\n";
+	for (size_t i = 0; i < len; i++)
+		func(arr[i]);
+}
+
 
 #endif
--- module_07/ex01/main.cpp
+++ module_07/ex01/main.cpp
@@ -6,18 +6,25 @@
 /*   By: atyurina <atyurina@student.42london.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/14 20:21:47 by atyurina          #+#    #+#             */
-/*   Updated: 2024/10/14 20:56:13 by atyurina         ###   ########.fr       */
+/*   Updated: 2026/01/12 21:47:53 by atyurina         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "iter.hpp"
 
-void	IntIncreaseAndPrint(int n)
+void	IntIncreaseAndPrint(int &n)
 {
-	std::cout << "number = " << n << "; increased number = " << n + 1 << std::endl;
+	std::cout << "number = " << n << ";";
+	n = n + 1;
+	std::cout << "increased number = " << n << std::endl;
 }
 
-void	CharToUpperAndPrint(char c)
+void	IntPrint(const int &n)
+{
+	std::cout << "number = " << n << std::endl;
+}
+
+void	CharToUpperAndPrint(char &c)
 {
 	c = toupper(c);
 	std::cout << c;
@@ -26,12 +33,15 @@ void	CharToUpperAndPrint(char c)
 int	main()
 {
 	int arr[] = {0, 1, 2, 3, 4, 5};
-	int	len = 6;
+	int	const len = 6;
 
+	iter(arr, len, IntPrint);
 	iter(arr, len, IntIncreaseAndPrint);
 
 	char arr2[] = {"Hello World"};
-	int len2 = 11;
+	int const len2 = 11;
 
 	iter(arr2, len2, CharToUpperAndPrint);
+
+	std::cout << "\n";
 }
