55724f8b3f8c474f6bce998d65613c1b5322d670
Arina <atyurina@student.42london.com>
2026-01-19 18:46:26 +0000

    added comments

--- module_08/ex00/easyfind.hpp
+++ module_08/ex00/easyfind.hpp
@@ -6,7 +6,7 @@
 /*   By: atyurina <atyurina@student.42london.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/15 23:09:11 by atyurina          #+#    #+#             */
-/*   Updated: 2024/10/16 00:12:56 by atyurina         ###   ########.fr       */
+/*   Updated: 2026/01/19 18:45:18 by atyurina         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,15 +19,17 @@
 #include <list>
 
 template <typename T>
-typename T::iterator easyfind(T container, int val)
+typename T::iterator easyfind(T& container, int val)
 {
-	typename T::iterator	it;
+	typename T::iterator	it; //points to an element inside container and behaves like a pointer
 	it = std::find(container.begin(), container.end(), val);
-	if (it == container.end())
+	//returns iterator to the first matching element OR container.end() if not found
+	if (it == container.end()) //end points one past the last element
 		throw std::out_of_range("Container does not contain passed value");
 	else
 	{
 		int index = std::distance(container.begin(), it);
+		//Counts how many steps it takes to go from begin() to it
 		std::cout << "Value was successfully found at index: " << index << std::endl;
 		return (it);
 	}
